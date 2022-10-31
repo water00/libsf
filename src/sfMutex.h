40f8f0bfa7eef1fa9438c9379c5a205590ef7b42
Kuppam Indrakumar <ikumar00@gmail.com>
2022-10-30 19:50:03 -0700

    Removed commented code

--- src/sfMutex.h
+++ src/sfMutex.h
@@ -7,65 +7,6 @@
 #include <condition_variable>
 #include "sfDebug.h"
 
-/*
-class SFMutex
-{
-protected:
-    std::mutex mutex;
-    std::condition_variable condVar;
-    std::atomic_bool inProcess;
-    std::atomic_bool startProcess;
-
-public:
-    SFMutex() 
-    {
-        inProcess = false;
-        startProcess = true;
-    }
-
-    virtual ~SFMutex()
-    {
-    }
-
-    inline void lock() { std::lock_guard<std::mutex> lock(mutex); }
-
-    inline void wait_forProcessEnd()
-    {
-        std::unique_lock<std::mutex> lk(mutex);
-        while(inProcess)
-        {
-            condVar.wait(lk);
-        }
-        startProcess = false;
-        condVar.notify_one();
-    }
-
-    inline void restart_process()
-    {
-        std::unique_lock<std::mutex> lk(mutex);
-        startProcess = true;
-        condVar.notify_one();
-    }
-
-    inline void wait_forProcessStart()
-    {
-        std::unique_lock<std::mutex> lk(mutex);
-        while(!startProcess)
-        {
-            condVar.wait(lk);
-        }
-        inProcess = true;
-        condVar.notify_one();
-    }
-
-    inline void end_process()
-    {
-        std::unique_lock<std::mutex> lk(mutex);
-        inProcess = false;
-        condVar.notify_all();
-    }
-};
-*/
 
 class SFMutex
 {
